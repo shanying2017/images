fde3326dce93a09c9aed2131283222537fbafb8d
shanying2017 <shanying2008@163.com>
2020-09-20 23:10:28 +0800

    Update download-data.sh

--- download-data.sh
+++ download-data.sh
@@ -32,7 +32,8 @@ mkdir -p "${KITTI_PATH}" "${GENERATED_KITTI_PATH}"
 
 # Download files
 unpack_kitti_file() {
-  wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"$1"/"$2" && unzip "$2"  -d "$3" && rm "$2"
+  #wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"$1"/"$2" && unzip "$2"  -d "$3" && rm "$2"
+  wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"$1"/"$2" && unzip "$2"  -d "$3"
 }
 
 subdircount=$(find ${KITTI_PATH} -maxdepth 1 -type d | wc -l)
@@ -50,7 +51,8 @@ fi
 
 unpack_kitti_calib_file() {
   local calib_file="${1}_calib.zip"
-  wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"$calib_file" && unzip "$calib_file"  -d "$2" && rm "$calib_file"
+  #wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"$calib_file" && unzip "$calib_file"  -d "$2" && rm "$calib_file"
+  wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/"$calib_file" && unzip "$calib_file"  -d "$2"
 }
 
 if [ ! -f "${KITTI_PATH}/${KITTI_DATE}/calib_imu_to_velo.txt" ]; then
